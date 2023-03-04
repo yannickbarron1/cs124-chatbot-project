246f90eb7c13403ec810d04f789ce46a37193da8
yannickbarron1 <60899560+yannickbarron1@users.noreply.github.com>
2023-03-04 11:59:23 -0800

    removed comment

--- chatbot.py
+++ chatbot.py
@@ -15,7 +15,7 @@ class Chatbot:
     def __init__(self, creative=False):
         # The chatbot's default name is `moviebot`.
         # TODO: Give your chatbot a new name.
-        self.name = 'moviebot' #comment
+        self.name = 'moviebot'
 
         self.creative = creative
 
