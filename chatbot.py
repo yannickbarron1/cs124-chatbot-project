534e791035195467460eecf1a624ee01a297e383
Sang <sang.ahn.94@gmail.com>
2023-03-09 00:44:20 -0800

    added disambiguate

--- chatbot.py
+++ chatbot.py
@@ -638,7 +638,24 @@ class Chatbot:
         :returns: a list of indices corresponding to the movies identified by
         the clarification
         """
-        pass
+        matches = []
+        if re.fullmatch(r'\d',clarification):
+            for i in range(len(candidates)):
+                if clarification in self.list_title[candidates[i]][0]:
+                    matches.append(candidates[i])
+        elif re.fullmatch(r'\d\d',clarification) or re.fullmatch(r'\d\d\d\d',clarification):            
+            for i in range(len(candidates)):
+                if clarification in self.list_title[candidates[i]][-1]:
+                    matches.append(candidates[i])
+        else:
+            for i in range(len(candidates)):
+                a = re.sub(r'[^\w\s]', '',clarification).lower()
+                b = re.sub(r'[^\w\s]', '',self.longstring_title[candidates[i]]).lower()
+                if a in b:
+                    matches.append(candidates[i])
+        if len(matches)==0:
+            matches = []
+        return matches
 
     ############################################################################
     # 3. Movie Recommendation helper functions                                 #
